f7433bfc70e83d0312a1e8520c07a6b8be83799a
Mavaebrook <starcheredward0@gmail.com>
2026-04-20 18:46:38 -0400

    Update TransitApplication.kt

--- app/src/main/kotlin/com/handleit/transit/app/TransitApplication.kt
+++ app/src/main/kotlin/com/handleit/transit/app/TransitApplication.kt
@@ -2,6 +2,7 @@ package com.handleit.transit.app
 
 import android.app.Application
 import com.handleit.transit.common.ConfigValidator
+import com.handleit.transit.common.TransitConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,6 +10,7 @@ import timber.log.Timber
 class TransitApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        TransitConfig.MAPS_API_KEY = BuildConfig.GOOGLE_MAP_KEY
         Timber.plant(Timber.DebugTree())
         ConfigValidator.validate(isDebug = true)
     }
