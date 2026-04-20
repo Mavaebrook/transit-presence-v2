d23947ccf7c5e86c399ed6546a148a4cbdd6083e
Mavaebrook <starcheredward0@gmail.com>
2026-04-20 18:50:49 -0400

    Update build.gradle.kts

--- core/core-common/build.gradle.kts
+++ core/core-common/build.gradle.kts
@@ -9,14 +9,3 @@ kotlin {
 dependencies {
     testImplementation(libs.junit)
 }
-android {
-    buildFeatures {
-        buildConfig = true
-    }
-    defaultConfig {
-        // Reads from the environment variable 'MAPS_API_KEY' if it exists, 
-        // otherwise uses a placeholder string.
-        val mapsKey: String = System.getenv("MAPS_API_KEY") ?: "AIzaSyC9HCFtqOrR9ZCwIisVm-FJ5OIeU88Ort0"
-        buildConfigField("String", "MAPS_API_KEY", "\"$mapsKey\"")
-    }
-}
