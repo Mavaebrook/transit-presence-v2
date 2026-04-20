2ee84b869dee3ca46a4db85cf9116e2de5c0a679
Mavaebrook <starcheredward0@gmail.com>
2026-04-20 19:39:57 -0400

    Delete feature/feature-map/src/main/kotlin/com/handleit/transit/feature/map/MapUiState.kt

--- feature/feature-map/src/main/kotlin/com/handleit/transit/feature/map/MapUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.handleit.transit.feature.map
-
-import com.handleit.transit.model.Vehicle
-
-data class MapUiState(
-    val vehicles: List<Vehicle> = emptyList(),
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
