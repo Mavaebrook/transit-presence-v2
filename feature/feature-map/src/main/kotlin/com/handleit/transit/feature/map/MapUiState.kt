8bac6820a6e80a6f03a2778f67ea21cfbe3b56ec
Mavaebrook <starcheredward0@gmail.com>
2026-04-20 19:32:04 -0400

    Update MapUiState.kt

--- feature/feature-map/src/main/kotlin/com/handleit/transit/feature/map/MapUiState.kt
+++ feature/feature-map/src/main/kotlin/com/handleit/transit/feature/map/MapUiState.kt
@@ -1,3 +1,7 @@
+package com.handleit.transit.feature.map
+
+import com.handleit.transit.model.Vehicle
+
 data class MapUiState(
     val vehicles: List<Vehicle> = emptyList(),
     val isLoading: Boolean = false,
