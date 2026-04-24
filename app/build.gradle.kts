c7c0ce774258e22b4373e9f5f695cec452f4a14e
Mavaebrook <starcheredward0@gmail.com>
2026-04-24 15:49:46 -0500

    debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,6 @@ dependencies {
     // --- FIX: Missing DI types for AppModule ---
     // Location Services (Provides FusedLocationProviderClient)
     implementation("com.google.android.gms:play-services-location:21.2.0")
-    implementation(project(":feature:feature-debug"))
 
     // Networking (Provides OkHttpClient & Logging)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
